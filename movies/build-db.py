a8e734d53854ca19c02952f3cad1ad7de751b2c8
vivekduttamishra <vivek@conceptarchitect.in>
2017-11-05 20:38:31 +0530

    Movies Data Added

---
+++ movies/build-db.py
@@ -0,0 +1,22 @@
+import json
+
+src='./search/movies.json'
+trgt='./search/movies-rating.json'
+details_folder='./details/'
+count=0
+
+with open(src) as sfile:
+    data=json.load(sfile)
+    for rec in data['Search']:
+        count+=1
+        id=rec['imdbID']
+        details_path= '{}{}.json'.format(details_folder,id)
+        with open(details_path) as dfile:
+            details=json.load(dfile)
+            rating=details['imdbRating']
+        rec['imdbRating']=rating
+
+with open(trgt,'w') as tfile:
+    json.dump(data,tfile)
+
+print('{} records updated'.format(count))
