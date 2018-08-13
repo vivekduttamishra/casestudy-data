b6c4da2bc9f15638d2a6f357dccce6ac4230fcf0
vivekduttamishra <vivek@conceptarchitect.in>
2018-08-13 22:10:50 +0530

    missing movie image updated

---
+++ movies/add-date-earning.py
@@ -0,0 +1,25 @@
+import json
+
+src='./search/movies-rating.json'
+trgt='./search/movies-dummy.json'
+details_folder='./details/'
+count=0
+
+
+
+with open(src) as sfile:
+    data=json.load(sfile)
+    for rec in data['Search']:
+        count+=1
+        id=rec['imdbID']
+        details_path= '{}{}.json'.format(details_folder,id)
+        with open(details_path) as dfile:
+            details=json.load(dfile)
+            rating=details['imdbRating']
+        rec['imdbRating']=rating
+        rec['Poster']='/images/{}.jpg'.format(id);
+
+with open(trgt,'w') as tfile:
+    json.dump(data,tfile)
+
+print('{} records updated'.format(count))
